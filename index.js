7fc5d1df34beb3c1fbfe0e0abd82def04a1511e6
endri xhukellari <32928261+endrixh7@users.noreply.github.com>
2023-10-19 12:20:39 +0200

    Continue with API

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-// Store url on a variable
-const string = 'https://jsonplaceholder.typicode.com/users';
+// Multiple Request Management
+// Talk to multiple servers
 
+const jsonstr = 'https://random-data-api.com/api/v2/users?size=10';
+const imgstr = 'https://picsum.photos/id/237/300/200';
 
-//Create a function
-async function getData(){
-    // Make a Fetch Call
-    const url = new URL(string);
-    const request = new Request(url, {
-        headers: { 'x-endri': 'hello'},
-        method: 'GET',
-        cache: 'no-store'
-    });
+// Create the function to make the Fetch request
+// Get all data at the same time
+// Promise object has a method - .Promise.all();
+// .race() -> I will give an array of promises
+// .allSettled() -> Give me the array when everything is succesfully done
 
-    fetch(request)
-    .then((resp)=>{
-        return resp.json();
-    })
-    .then((data)=>{
-        console.log(data);
-    })
-    .catch((error)=> console.log(error));
-    
-}
-
+export function getData() {
+    //
+    let imgResponse;
+    let jsonResponse;
+    //sequence
+    fetch(imgstr)
+      .then((response) => {
+        if (!response.ok) throw new Error('invalid');
+        return response.blob();
+        //we can add another then and work with image here
+      })
+      .then((blob) => {
+        //work with blob add img to page...
+        return fetch(jsonstr);
+      })
+      .then((response) => {
+        if (!response.ok) throw new Error('invalid');
+        jsonResponse = response.json();
+        return Promise.all([imgResponse, jsonResponse]);
+      })
+      .then(([blob, dataObj]) => {
+        console.log(dataObj);
+        console.log(blob);
+      })
+      .catch(console.warn);
+  
+    Promise.all([fetch(imgstr), fetch(jsonstr)])
+      .then(([imgRes, jsonRes]) => {
+        return Promise.all([imgRes.blob(), jsonRes.json()]);
+      })
+      .then(([blob, jsonData]) => {
+        //at the same time
+      })
+      .catch(console.warn);
+  }
 
 getData();
